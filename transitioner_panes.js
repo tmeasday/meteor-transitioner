9a562ef980298c7e27b7493b33df0156f2d0238c
Tom Coleman <tom@thesnail.org>
2013-05-30 08:36:52 +1000

    Removed spurious console.logging.

--- transitioner_panes.js
+++ transitioner_panes.js
@@ -1,6 +1,5 @@
 Template.transitionerFirstPane.firstPane = function() {
   var template = Template[Meteor.Transitioner.firstPane()];
-  console.log(template);
   return template ? template() : '';
 }
 Template.transitionerSecondPane.secondPane = function() {
